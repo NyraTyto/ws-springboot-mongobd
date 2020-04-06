656a3e6b29442bd74aad4ccc1ad1ad603139c311
Juana Paiva <juanapaiva2019@gmail.com>
2020-04-06 13:42:56 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/juanapaiva/workshopmongo/WorkshopsmongoApplication.java
@@ -0,0 +1,13 @@
+package com.juanapaiva.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopsmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopsmongoApplication.class, args);
+	}
+
+}
